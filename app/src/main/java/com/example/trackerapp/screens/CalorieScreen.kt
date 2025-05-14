4a4f6930623d17ff11fa330e749bb1284f2da73a
Adem Mustajbasic <156593541+MAdem01@users.noreply.github.com.>
2025-05-14 15:01:10 +0200

    add CustomBottomAppBar

--- app/src/main/java/com/example/trackerapp/screens/CalorieScreen.kt
+++ app/src/main/java/com/example/trackerapp/screens/CalorieScreen.kt
@@ -2,12 +2,17 @@ package com.example.trackerapp.screens
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.example.trackerapp.composables.CustomBottomAppBar
 
 @Composable
 fun CalorieScreen(){
     Scaffold(
-        bottomBar = { CustomBottomAppBar() },
+        bottomBar = { CustomBottomAppBar(
+            calorieIconColor = Color.White,
+            workoutIconColor = Color.LightGray,
+            accountIconColor = Color.LightGray
+        ) },
     ) { paddingValues ->
     }
 }
